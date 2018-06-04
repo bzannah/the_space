bd4c0ad28faa7ddebc82f24462c4a68d08087cce
Babagana Zannah <babagana.tech@gmail.com>
2018-06-04 10:25:07 +0200

    order comment by when they were created

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -59,6 +59,7 @@ class Article
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="article")
+     * @ORM\OrderBy({"createdAt" = "DESC"})
      */
     private $comments;
 
