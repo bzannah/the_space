d6f63909aa0bed7ced73d72f9baab18a849cda62
Babagana Zannah <babagana.tech@gmail.com>
2018-12-01 16:13:48 +0100

    update files

--- config/bundles.php
+++ config/bundles.php
@@ -3,8 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
@@ -16,7 +14,6 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
-    Nexy\SlackBundle\NexySlackBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\CommentRepository;
-use App\Service\MarkdownHelper;
-use App\Service\SlackClient;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -49,12 +47,8 @@ class ArticleController extends AbstractController
      * VIPNOTE: lazy loading: related data is not queried for until, and unless, we use it.
      * @throws \Http\Client\Exception
      */
-    public function show(Article $article, SlackClient $slack)
+    public function show(Article $article)
     {
-        if ($article->getSlug() == 'slack') {
-            $slack->sendMessage('John Doe', 'A message from SLACK');
-        }
-
         return $this->render('articles/show.html.twig', [
             'article' => $article
         ]);
